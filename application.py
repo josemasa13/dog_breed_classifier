d3e7fc13b47cfa2ac7a1675191f91533c0adacc2
Marcial Sanchez Jose Alberto <jose.marcial@solistica.com>
2020-05-14 20:58:08 -0500

    Add gitignore file

--- application.py
+++ application.py
@@ -1,3 +1,10 @@
+import json
+import pandas as pd
+import os
+from flask import Flask
+from flask import render_template, request, jsonify
+import tempfile
+from werkzeug.utils import secure_filename
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import model_from_json
@@ -147,3 +154,38 @@ def predict_image(img_path):
 
     else:    
         return "It seems that this image doesn't contain a human or a dog, please try again"
+     
+
+UPLOAD_FOLDER = './images/'
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('master.html')
+
+@app.route('/go', methods = ['GET', 'POST'])
+def go():
+    # save user input in query
+
+    # use model to predict classification for query
+    if request.method == 'POST':
+        f = request.files['file']
+        filename = secure_filename(f.filename)
+        print(filename)
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        res = predict_image(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+    # This will render the go.html Please see that file. 
+    return render_template(
+        'go.html',
+        result=res
+    )
+
+def main():
+    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
